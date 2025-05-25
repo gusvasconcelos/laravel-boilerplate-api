3160216f7938e78b2dd9a335f88443b4282a8854
Gustavo Vasconcelos <gusvasconcelos@proton.me>
2025-05-25 00:41:03 -0300

    feat: implementing error handling and api route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,12 +1,25 @@
 <?php
 
+use Illuminate\Http\Request;
+use App\Exceptions\HttpException;
 use Illuminate\Foundation\Application;
+use App\Helpers\Response\ErrorResponse;
+use Illuminate\Database\QueryException;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -14,5 +27,101 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->dontReport([
+            InvalidArgumentException::class,
+            HttpException::class,
+            JWTException::class,
+        ]);
+
+        $exceptions->render(function (ValidationException $e) {
+            return (
+                new ErrorResponse($e, 'Erro na validação de campo.', 422, 'VALIDATION', $e->errors())
+            )->toJson();
+        });
+
+        $exceptions->render(function (InvalidArgumentException $e) {
+            return (
+                new ErrorResponse($e, $e->getMessage(), 422, 'INVALID_ARGUMENT')
+            )->toJson();
+        });
+
+        $exceptions->render(function (AuthorizationException $e) {
+            return (
+                new ErrorResponse($e, 'Não autorizado.', 403, 'FORBIDDEN')
+            )->toJson();
+        });
+
+        $exceptions->render(function (ModelNotFoundException $e) {
+            $model = $e->getModel();
+
+            $ids = implode(', ', $e->getIds());
+
+            return (
+                new ErrorResponse($e, "$model não encontrado(a).", 404, 'RESOURCE_NOT_FOUND', ['IDs: ' => $ids])
+            )->toJson();
+        });
+
+        $exceptions->render(function (QueryException $e) {
+            return (
+                new ErrorResponse($e, 'Não foi possível executar a instrução no banco de dados.', 406, 'QUERY_NOT_ACCEPTABLE')
+            )->toJson();
+        });
+
+        $exceptions->render(function (NotFoundHttpException $e, Request $req) {
+            $path = $req->path();
+
+            return (
+                new ErrorResponse($e, "A rota $path não foi encontrada.", 404, 'RESOURCE_NOT_FOUND')
+            )->toJson();
+        });
+
+        $exceptions->render(function (MethodNotAllowedHttpException $e, Request $req) {
+            $method = $req->method();
+            $path = $req->path();
+            $allowedMethods = $e->getHeaders()['Allow'];
+
+            return (
+                new ErrorResponse(
+                    $e,
+                    "O método $method não é suportado para a rota $path. Métodos suportados: $allowedMethods",
+                    405,
+                    'METHOD_NOT_ALLOWED'
+                )
+            )->toJson();
+        });
+
+        $exceptions->render(function (TokenInvalidException $e) {
+            return (
+                new ErrorResponse($e, 'Token inválido.', 403, 'AUTH_INVALID_TOKEN')
+            )->toJson();
+        });
+
+        $exceptions->render(function (TokenExpiredException $e) {
+            return (
+                new ErrorResponse($e, 'Token expirado.', 401, 'AUTH_EXPIRED_TOKEN')
+            )->toJson();
+        });
+
+        $exceptions->render(function (JWTException $e) {
+            return (
+                new ErrorResponse(
+                    $e,
+                    'O token não pôde ser analisado a partir da solicitação.',
+                    401,
+                    'AUTH_JWT_ERROR'
+                )
+            )->toJson();
+        });
+
+        $exceptions->render(function (HttpException $e) {
+            return (
+                new ErrorResponse($e, $e->getMessage(), $e->getStatusCode(), $e->getErrorCode(), $e->getDetails())
+            )->toJson();
+        });
+
+        $exceptions->render(function (Throwable $e) {
+            return (
+                new ErrorResponse($e, $e->getMessage(), 500, 'INTERNAL_SERVER')
+            )->toJson();
+        });
     })->create();
