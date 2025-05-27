01324d058ecfd3deccf8f2c87d8a5cc0f0d51799
Gustavo Vasconcelos <gusvasconcelos@proton.me>
2025-05-26 21:52:36 -0300

    refactor: ajusts in code style after define rules

--- app/Helpers/Response/ErrorResponse.php
+++ app/Helpers/Response/ErrorResponse.php
@@ -92,4 +92,3 @@ class ErrorResponse implements Arrayable
             })->toArray();
     }
 }
-
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,6 @@ namespace Tests;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Tymon\JWTAuth\Facades\JWTAuth;
 
 abstract class TestCase extends BaseTestCase
 {
