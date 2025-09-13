f140173268e9117e16ad3a59071760f8a02777e8
Gustavo Vasconcelos <gusvasconcelos@proton.me>
2025-09-13 09:44:18 -0300

    feat: enhance error handling by adding request ID to error responses and updating response methods

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,12 @@ use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Tymon\JWTAuth\Exceptions\TokenExpiredException;
 use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Illuminate\Support\Str;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -33,41 +35,46 @@ return Application::configure(basePath: dirname(__DIR__))
             JWTException::class,
         ]);
 
-        $exceptions->render(function (ValidationException $e) {
+        $reqId = Str::uuid7()->toString();
+
+        $exceptions->render(function (ValidationException $e) use ($reqId) {
             $errorResponse = new ErrorResponse(
                 exception: $e,
                 message: __('errors.validation'),
                 statusCode: 422,
                 errorCode: 'VALIDATION',
+                reqId: $reqId,
                 details: $e->errors()
             );
 
-            return $errorResponse->toJson();
+            return $errorResponse->toJsonResponse();
         });
 
-        $exceptions->render(function (InvalidArgumentException $e) {
+        $exceptions->render(function (InvalidArgumentException $e) use ($reqId) {
             $errorResponse = new ErrorResponse(
                 exception: $e,
                 message: $e->getMessage(),
                 statusCode: 422,
-                errorCode: 'INVALID_ARGUMENT'
+                errorCode: 'INVALID_ARGUMENT',
+                reqId: $reqId,
             );
 
-            return $errorResponse->toJson();
+            return $errorResponse->toJsonResponse();
         });
 
-        $exceptions->render(function (AuthorizationException $e) {
+        $exceptions->render(function (AuthorizationException $e) use ($reqId) {
             $errorResponse = new ErrorResponse(
                 exception: $e,
                 message: __('errors.unauthorized'),
                 statusCode: 403,
-                errorCode: 'FORBIDDEN'
+                errorCode: 'FORBIDDEN',
+                reqId: $reqId,
             );
 
-            return $errorResponse->toJson();
+            return $errorResponse->toJsonResponse();
         });
 
-        $exceptions->render(function (ModelNotFoundException $e) {
+        $exceptions->render(function (ModelNotFoundException $e) use ($reqId) {
             $model = $e->getModel();
 
             $ids = implode(', ', $e->getIds());
@@ -77,37 +84,45 @@ return Application::configure(basePath: dirname(__DIR__))
                 message: __('errors.resource_not_found', ['resource' => $model]),
                 statusCode: 404,
                 errorCode: 'RESOURCE_NOT_FOUND',
+                reqId: $reqId,
                 details: ['IDs: ' => $ids]
             );
 
-            return $errorResponse->toJson();
+            return $errorResponse->toJsonResponse();
         });
 
-        $exceptions->render(function (QueryException $e) {
+        $exceptions->render(function (QueryException $e) use ($reqId) {
             $errorResponse = new ErrorResponse(
                 exception: $e,
                 message: __('errors.query_not_acceptable'),
                 statusCode: 406,
-                errorCode: 'QUERY_NOT_ACCEPTABLE'
+                errorCode: 'QUERY_NOT_ACCEPTABLE',
+                reqId: $reqId,
             );
 
-            return $errorResponse->toJson();
+            Log::critical(
+                message: 'Erro de query no sistema',
+                context: $errorResponse->toArray()
+            );
+
+            return $errorResponse->toJsonResponse();
         });
 
-        $exceptions->render(function (NotFoundHttpException $e, Request $req) {
+        $exceptions->render(function (NotFoundHttpException $e, Request $req) use ($reqId) {
             $path = $req->path();
 
             $errorResponse = new ErrorResponse(
                 exception: $e,
                 message: __('errors.route_not_found', ['route' => $path]),
                 statusCode: 404,
-                errorCode: 'RESOURCE_NOT_FOUND'
+                errorCode: 'RESOURCE_NOT_FOUND',
+                reqId: $reqId,
             );
 
-            return $errorResponse->toJson();
+            return $errorResponse->toJsonResponse();
         });
 
-        $exceptions->render(function (MethodNotAllowedHttpException $e, Request $req) {
+        $exceptions->render(function (MethodNotAllowedHttpException $e, Request $req) use ($reqId) {
             $method = $req->method();
             $path = $req->path();
             $allowedMethods = $e->getHeaders()['Allow'];
@@ -116,76 +131,88 @@ return Application::configure(basePath: dirname(__DIR__))
                 exception: $e,
                 message: __('errors.method_not_allowed', ['method' => $method, 'route' => $path, 'allowedMethods' => $allowedMethods]),
                 statusCode: 405,
-                errorCode: 'METHOD_NOT_ALLOWED'
+                errorCode: 'METHOD_NOT_ALLOWED',
+                reqId: $reqId,
             );
 
-            return $errorResponse->toJson();
+            return $errorResponse->toJsonResponse();
         });
 
-        $exceptions->render(function (TokenInvalidException $e) {
+        $exceptions->render(function (TokenInvalidException $e) use ($reqId) {
             $errorResponse = new ErrorResponse(
                 exception: $e,
                 message: __('errors.auth_invalid_token'),
                 statusCode: 403,
-                errorCode: 'AUTH_INVALID_TOKEN'
+                errorCode: 'AUTH_INVALID_TOKEN',
+                reqId: $reqId,
             );
 
-            return $errorResponse->toJson();
+            return $errorResponse->toJsonResponse();
         });
 
-        $exceptions->render(function (TokenExpiredException $e) {
+        $exceptions->render(function (TokenExpiredException $e) use ($reqId) {
             $errorResponse = new ErrorResponse(
                 exception: $e,
                 message: __('errors.auth_token_expired'),
                 statusCode: 401,
-                errorCode: 'AUTH_EXPIRED_TOKEN'
+                errorCode: 'AUTH_EXPIRED_TOKEN',
+                reqId: $reqId,
             );
 
-            return $errorResponse->toJson();
+            return $errorResponse->toJsonResponse();
         });
 
-        $exceptions->render(function (TokenExpiredException $e) {
+        $exceptions->render(function (TokenExpiredException $e) use ($reqId) {
             $errorResponse = new ErrorResponse(
                 exception: $e,
                 message: __('errors.auth_token_expired'),
                 statusCode: 401,
-                errorCode: 'AUTH_EXPIRED_TOKEN'
+                errorCode: 'AUTH_EXPIRED_TOKEN',
+                reqId: $reqId,
             );
 
-            return $errorResponse->toJson();
+            return $errorResponse->toJsonResponse();
         });
 
-        $exceptions->render(function (JWTException $e) {
+        $exceptions->render(function (JWTException $e) use ($reqId) {
             $errorResponse = new ErrorResponse(
                 exception: $e,
                 message: __('errors.auth_jwt_error'),
                 statusCode: 401,
-                errorCode: 'AUTH_JWT_ERROR'
+                errorCode: 'AUTH_JWT_ERROR',
+                reqId: $reqId,
             );
 
-            return $errorResponse->toJson();
+            return $errorResponse->toJsonResponse();
         });
 
-        $exceptions->render(function (HttpException $e) {
+        $exceptions->render(function (HttpException $e) use ($reqId) {
             $errorResponse = new ErrorResponse(
                 exception: $e,
                 message: $e->getMessage(),
                 statusCode: $e->getStatusCode(),
                 errorCode: $e->getErrorCode(),
+                reqId: $reqId,
                 details: $e->getDetails()
             );
 
-            return $errorResponse->toJson();
+            return $errorResponse->toJsonResponse();
         });
 
-        $exceptions->render(function (Throwable $e) {
+        $exceptions->render(function (Throwable $e) use ($reqId) {
             $errorResponse = new ErrorResponse(
                 exception: $e,
                 message: __('errors.internal_server'),
                 statusCode: 500,
-                errorCode: 'INTERNAL_SERVER'
+                errorCode: 'INTERNAL_SERVER',
+                reqId: $reqId,
             );
 
-            return $errorResponse->toJson();
+            Log::critical(
+                message: 'Erro interno no sistema',
+                context: $errorResponse->toArray()
+            );
+
+            return $errorResponse->toJsonResponse();
         });
     })->create();
