55682b1b7d806ad0a1747adbb3e4f434311e7185
Gustavo Vasconcelos <gusvasconcelos@proton.me>
2025-09-14 12:09:48 -0300

    feat: adicionar documentação da API com Swagger e rota para visualização

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use OpenApi\Attributes\Info;
+use OpenApi\Attributes\Server;
+use OpenApi\Attributes\SecurityScheme;
+use OpenApi\Attributes\Tag;
+
+#[Info(
+    version: '1.0.0',
+    title: 'Laravel Boilerplate API',
+    description: 'Laravel Boilerplate API Documentation'
+)]
+#[Server(
+    url: '/api/v1',
+    description: 'Laravel Boilerplate API V1 Server'
+)]
+#[SecurityScheme(
+    securityScheme: 'token',
+    type: 'http',
+    scheme: 'bearer',
+    bearerFormat: 'JWT',
+    description: 'Token JWT for authentication. Add the token without the "Bearer" prefix'
+)]
+#[Tag(
+    name: 'Authentication',
+    description: 'Authentication endpoints'
+)]
 abstract class Controller
 {
     //
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/docs', function () {
+    $url = url('api/v1/docs');
+
+    return view('swagger-ui', compact('url'));
+});
