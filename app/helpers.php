b531dbcef1627500ca00f69172b76e0f5b09b6fc
Gustavo Vasconcelos <gusvasconcelos@proton.me>
2025-09-20 12:48:07 -0300

    refactor: replace function 'json_pretty' to 'cast' with anonymous class with method 'toJsonPretty'

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,12 @@
 <?php
 
-if (!function_exists('json_pretty')) {
-    function json_pretty($data): string {
-        return json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+if (! function_exists('cast')) {
+    function cast() {
+        return new class {
+            public function toJsonPretty(array $data): string
+            {
+                return json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+            }
+        };
     }
 }
